1961048c414220502e6e87b9f09e538e7e157084
N Kort <studio@n-kort.net>
2023-05-05 11:38:11 +0100

    change background and fog

--- src/map/Canvas.tsx
+++ src/map/Canvas.tsx
@@ -22,7 +22,8 @@ export default function ({ onObjLoaded }: { onObjLoaded: () => void }) {
       <ObjectMesh onObjLoaded={onObjLoaded} castShadow receiveShadow/>
 
       <pointLight position={[-600, -500, 5000]} color={0xffffff} intensity={0.6}/>
-      <fog attach='fog' args={[0x988C99, 700, 4400]}/>
+      {/* <fog attach='fog' args={[0x988C99, 700, 4400]}/> */}
+      <fog attach='fog' args={[0xBFA16A, 700, 4400]}/>
     </Canvas>
   )
 }
